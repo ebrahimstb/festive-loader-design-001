990ac488a15490d0c4604a5b51fb0a4b42a30d05
ebrahimstb <azeezhibro006@gmail.com>
2024-12-04 12:54:24 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Loader 1: Spinning Snowflake
+document.getElementById('loader1').innerHTML = `<div class="snowflake">❄</div>`;
+
+// Loader 2: Twinkling Christmas Lights
+document.getElementById('loader2').innerHTML = `
+  <div class="lights">
+    <span></span><span></span><span></span><span></span>
+  </div>`;
+
+// Loader 3: Gift Box Animation
+document.getElementById('loader3').innerHTML = `
+  <div class="gift-box"></div>`;
+
+// Loader 4: Waving Santa Hat
+document.getElementById('loader4').innerHTML = `<div class="hat">🎅</div>`;
